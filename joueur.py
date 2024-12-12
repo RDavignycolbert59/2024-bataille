df9fc8af5e74b1476898942f86d2943e5ac73cc7
RDavignycolbert59 <rayandavigny8@gmail.com>
2024-12-12 09:28:44 +0100

    Update

--- joueur.py
+++ joueur.py
@@ -4,51 +4,25 @@ from structures import *
 def creer_joueur(nom: str) -> dict:
     # Crée un joueur avec un nom, une main vide et une pile de gains vide
     return {"nom": nom, "main": creer_pile(), "gain": creer_pile()}
-    
-def perdu(joueur:"JOUEUR") -> bool:
-    """
-        Un joueur a perdu quand sa main est vide et qu'il n'a pas de cartes gagnées
-        
-        :tests:
-        >>> j = creer_joueur("DAVID")
-        >>> perdu(j)
-        True
-        >>> empiler(j.main(creer_carte(0, 0)))
-        >>> perdu(j)
-        False
-    """
-    pass
 
-def recuperer_gain(joueur:"JOUEUR") -> None:
-    """
-        Récupère les cartes gagnées précédemment
-        On vide la pile de gain dans la pile de main
+def perdu(joueur: dict) -> bool:
+    # Vérifie si la main et la pile de gains du joueur sont toutes deux vides
+    return est_vide(joueur["main"]) and est_vide(joueur["gain"])
+
+def recuperer_gain(joueur: dict) -> None:
+    # Transfère les cartes de la pile de gains vers la pile de main
+    while not est_vide(joueur["gain"]):
+        empiler(joueur["main"], depiler(joueur["gain"]))
         
-        :tests:
-        >>> j = creer_joueur("DAVID")
-        >>> empiler(j.gain, creer_carte(0, 0))
-        >>> recuperer_gain(j)
-        >>> est_vide(j.gain)
-        True
-        >>> depiler(j.main)
-        Carte(valeur='7', symbole='♦')
-    """
-    pass
-        
-def jouer_une_carte(joueur):
-    """
-        Si le joueur n'a pas perdu :
-            - S'il n'a plus de carte dans sa main : il peut récupérer celles gagnées
-            - jouer la carte disponible
-            
-        :tests:
-        >>> j = creer_joueur("DAVID")
-        >>> jouer(j)
-        >>> empiler(j.gain, creer_carte(0, 0))
-        >>> jouer(j)
-        Carte(valeur='7', symbole='♦')
-    """       
-    pass
+def jouer_une_carte(joueur: dict):
+    # Si le joueur n'a pas perdu
+    if not perdu(joueur):
+        # Si la main est vide, récupérer les cartes gagnées
+        if est_vide(joueur["main"]):
+            recuperer_gain(joueur)
+        # Si des cartes sont disponibles, jouer la carte du sommet
+        if not est_vide(joueur["main"]):
+            return depiler(joueur["main"])
         
 if __name__ == '__main__':
     import doctest
